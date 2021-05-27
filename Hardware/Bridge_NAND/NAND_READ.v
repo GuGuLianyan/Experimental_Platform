e967920e6d5e230e324cc355d806ff51b45bd093
GuGuLianyan <nblianyan@outlook.com>
2021-05-27 15:17:12 +0800

    开发中

--- Hardware/Bridge_NAND/NAND_READ.v
+++ Hardware/Bridge_NAND/NAND_READ.v
@@ -3,7 +3,9 @@ module NAND_READ
 		parameter tWP = 2,
 		parameter tWH = 1,
 		parameter tHOLD = 1,
-		parameter tREA = 2
+		parameter tREA = 2,
+		parameter IO_DIR_OUT = 1,
+		parameter IO_DIR_IN = 0;
 	)
 	(
 		input wire CLK,
@@ -28,7 +30,7 @@ module NAND_READ
 
 reg[27:0] NAND_ADDR_reg;
 reg[7:0] CMD_reg;
-
+reg[15:0] Read_CNT;
 
 reg CMD_Send_Start;
 wire CMD_Send_Over;
@@ -87,7 +89,13 @@ NAND_RD
 		.ALE(RD_Data_ALE)
 	);
 	
-	
+
+reg Read_Status;
+parameter Read_Success		= 1'b0;
+parameter Read_Fail			= 1'b1;
+
+reg[7:0] IO_OUT_REG;
+
 reg[15:0] R_FSM_current;
 reg[15:0] R_FSM_next;
 
@@ -128,7 +136,149 @@ begin
 					begin
 						if(Start == 1)
 							begin
+								R_FSM_next = R_FSM_SEND_CMD_1st;
 							end
+						else
+							begin
+								R_FSM_next = R_FSM_IDLE;
+							end
+					end
+				R_FSM_SEND_CMD_1st  :
+					begin
+						if(CMD_Send_Over == 1)
+							begin
+								R_FSM_next = R_FSM_SEND_ADDR;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_1st;
+							end
+					end
+				R_FSM_SEND_ADDR	    :
+					begin
+						if(ADDR_Send_Over == 1)
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_2nd;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_SEND_ADDR;
+							end
+					end
+				R_FSM_SEND_CMD_2nd  :
+					begin
+						if(CMD_Send_Over == 1)
+							begin
+								R_FSM_next = R_FSM_Wait_RDY;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_2nd;
+							end
+					end
+				R_FSM_Wait_RDY	    :
+					begin
+						if(RDY_BSYn == 1)
+							begin
+								R_FSM_next = R_FSM_Wait_RDY;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_Wait_BSY;
+							end
+					end
+				R_FSM_Wait_BSY	    :
+					begin
+						if(RDY_BSYn == 1)
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_3rd;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_Wait_BSY;
+							end
+					end
+				R_FSM_SEND_CMD_3rd  :
+					begin
+						begin
+						if(CMD_Send_Over == 1)
+							begin
+								R_FSM_next = R_FSM_READ_ST;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_3rd;
+							end
+					end
+				R_FSM_READ_ST		:
+					begin
+						if(RD_DATA_Over == 1)
+							begin
+								if(Read_Status == Read_Success)
+									begin
+										R_FSM_next = R_FSM_SEND_CMD_4th;
+									end
+								else
+									begin
+										R_FSM_next = R_FSM_OVER;
+									end
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_READ_ST;
+							end
+					end
+				R_FSM_SEND_CMD_4th  :
+					begin
+						begin
+						if(CMD_Send_Over == 1)
+							begin
+								R_FSM_next = R_FSM_READ_DATA;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_SEND_CMD_4th;
+							end
+					end
+				R_FSM_READ_DATA	    :
+					begin
+						if(Read_CNT >= 2112)
+							begin
+								R_FSM_next = R_FSM_OVER;
+							end
+						else
+							begin
+								R_FSM_next = R_FSM_READ_DATA;
+							end
+					end
+				R_FSM_OVER		    :
+					begin
+						R_FSM_next = R_FSM_IDLE;
+					end
+				default:
+					begin
+						R_FSM_next = R_FSM_IDLE;
+					end
+			endcase
+		end
+end
+
+
+
+
+always@(posedge CLK or negedge RSTn)
+begin
+	if(RSTn == 0)
+		begin
+
+			
+		end
+	else
+		begin
+			case(R_FSM_current)
+				R_FSM_IDLE			:
+					begin
+						
 					end
 				R_FSM_SEND_CMD_1st  :
 					begin
@@ -170,9 +320,54 @@ begin
 					begin
 						
 					end
+				default:
+					begin
+						
+					end
 			endcase
 		end
 end
+
+
+assign IO = (IO_DIR == IO_DIR_OUT)? IO_OUT_REG : 8'hZZ;
+always@(*)
+begin
+	if(RSTn == 0)
+		begin
+			IO_DIR = IO_DIR_IN;
+			IO_OUT_REG  = 0;
+		end
+	else
+		begin
+			case(R_FSM_current)
+				R_FSM_SEND_CMD_1st, R_FSM_SEND_CMD_2nd, 
+				R_FSM_SEND_CMD_3rd, R_FSM_SEND_CMD_4th:
+					begin
+						IO_DIR = IO_DIR_OUT;
+						IO_OUT_REG = CMD_reg;
+					end
+				R_FSM_SEND_ADDR:
+					begin
+						IO_DIR = IO_DIR_OUT;
+						IO_OUT_REG = NAND_ADDR_WR;
+					end
+				default:
+					begin
+						IO_DIR = IO_DIR_IN;
+					end
+			endcase
+		end
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
